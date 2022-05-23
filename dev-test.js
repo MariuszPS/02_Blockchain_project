4ef71a95617af25cb23f56387b2c11c6c84d20d8
Mariusz PS <k8kpol@gmail.com>
2022-05-23 08:34:36 +0000

    genesis

--- dev-test.js
+++ dev-test.js
@@ -1,4 +1,11 @@
 
-const firstName = "Christof";
-console.log("Hallo, " + firstName);
+
+
+// Import
+const Block = require("./block");
+
+/***  Test der Block-Funktionalität  */
+const block = new Block("aktuelle Zeit","hash1","hash2","block data");
+console.log(block.toString());
+console.log(Block.genesis())
 
