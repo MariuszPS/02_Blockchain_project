0eb0812c4dd9d1a7e4392a2fd284ab81032d5eea
Mariusz PS <k8kpol@gmail.com>
2022-05-23 08:35:37 +0000

    genesis up

--- dev-test.js
+++ dev-test.js
@@ -7,5 +7,5 @@ const Block = require("./block");
 /***  Test der Block-Funktionalität  */
 const block = new Block("aktuelle Zeit","hash1","hash2","block data");
 console.log(block.toString());
-console.log(Block.genesis())
+console.log(Block.genesis().toString());
 
